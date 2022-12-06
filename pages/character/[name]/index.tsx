b2436371d8117b2d55004d9dbd6899f56e24fb18
Hannes Draba <H.Draba@porta.de>
2022-12-06 08:24:19 +0100

    type error fetching

--- pages/character/[name]/index.tsx
+++ pages/character/[name]/index.tsx
@@ -9,42 +9,46 @@ import { PRIVATE_API_KEY, PUBLIC_API_KEY } from '../../../private/keys';
 
 import md5 from 'md5';
 
-const ts = new Date().getTime();
-const newhash = md5(ts + PRIVATE_API_KEY + PUBLIC_API_KEY);
-let charData: any;
-
-let offset = 0;
-//   let marvelUrl = `https://gateway.marvel.com:443/v1/public/characters?name=hulk&apikey=${PUBLIC_API_KEY}`;
-// const marvelURL = `https://gateway.marvel.com/v1/public/characters?offset=${offset}&limit=100&ts=${ts}&apikey=${PUBLIC_API_KEY}&hash=${newhash}`;
-
-const getCharByName = async (url: string) => {
-  const response = await fetch(url);
-  const { data } = await response.json();
-  charData = data.results[0];
-  console.log(data);
+type SingleCharacter = {
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: { path: string; extension: string };
 };
+
+const timestamp = new Date().getTime();
+const newhash = md5(timestamp + PRIVATE_API_KEY + PUBLIC_API_KEY);
+
+let charData: SingleCharacter;
+
+let characterData: SingleCharacter;
 
 const Character = () => {
   const router = useRouter();
   const name = router.query.name;
 
-  let marvelUrl = `https://gateway.marvel.com/v1/public/characters?name=${name}&ts=${ts}&apikey=${PUBLIC_API_KEY}&hash=${newhash}`;
+  let marvelUrl = `https://gateway.marvel.com/v1/public/characters?name=${name}&ts=${timestamp}&apikey=${PUBLIC_API_KEY}&hash=${newhash}`;
 
   useEffect(() => {
-    getCharByName(marvelUrl).then(() => {
+    const getCharByName = async (url: string) => {
+      const response = await fetch(url);
+      const { data } = await response.json();
+      charData = data.results[0];
       console.log(charData);
-    });
-  }, []);
+      return charData;
+    };
+    characterData = getCharByName(marvelUrl);
+  }, [router]);
 
   return (
     <CharacterCard>
       <h1>{name}</h1>
-      {/* <h2>{charData.name}</h2> */}
-      {/* <img src={} alt={} /> */}
+      <h2>{characterData.name}</h2>
+      <img src={characterData.thumbnail.path} alt={characterData.name} />
     </CharacterCard>
   );
 };
 
 export default Character;
 
-// doesn't load on page load as useEffect should work, but when u change something and don't reload 
+// doesn't load on page load as useEffect should work, but when u change something and don't reload
