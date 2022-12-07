ff0e5889758d302ee24484b68fef6f8b31124d6d
Hannes Draba <H.Draba@porta.de>
2022-12-07 12:42:01 +0100

    added random character, error cors

---
+++ helper/fetchRandomCharacter.ts
@@ -0,0 +1,56 @@
+import md5 from 'md5';
+
+import { getTotalNumOfCharacters } from './getTotalNumberOfCharacters';
+import { PUBLIC_API_KEY, PRIVATE_API_KEY } from '.././private/keys';
+
+type Character = {
+  name: string;
+  description: string;
+  image: { thumbnail: { path: string; extension: string } };
+};
+
+let offset = 0;
+
+const idArray: string[] = [];
+// getRandomId
+const getRandomID = async () => {
+  const ts = new Date().getTime();
+  const newhash = md5(ts + PRIVATE_API_KEY + PUBLIC_API_KEY);
+  const totalNumOfCharacters = await getTotalNumOfCharacters(
+    ts,
+    PUBLIC_API_KEY,
+    newhash
+  );
+
+  while (idArray.length < totalNumOfCharacters) {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?offset=${offset}&limit=100&ts=${ts}&apikey=${PUBLIC_API_KEY}&hash=${newhash}`
+    );
+    const { data } = await response.json();
+    const characters = data.results;
+    for (let character of characters) {
+      idArray.push(character.id);
+    }
+    offset += 100;
+  }
+  const randomId =
+    idArray[Math.floor(Math.random() * totalNumOfCharacters - 1)];
+  return randomId;
+};
+
+export const fetchRandomCharacter = async () => {
+  const ts = new Date().getTime();
+  const newhash = md5(ts + PRIVATE_API_KEY + PUBLIC_API_KEY);
+  const id = await getRandomID();
+
+  const response = await fetch(
+    `https://gateway.marvel.com/v1/public/characters/${id}&apikey=${PUBLIC_API_KEY}`
+  );
+  const { data } = await response.json();
+  const character = data.results[0];
+  return {
+    name: character.name,
+    description: character.description,
+    imageUrl: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+  };
+};
--- pages/random-character/index.tsx
+++ pages/random-character/index.tsx
@@ -1,19 +1,48 @@
-import { useEffect } from "react"
+import { useEffect, useState } from 'react';
+import { CharacterCard } from '../../components/ui/CharacterCard';
+import { Spinner } from '../../components/ui/Spinner';
+import { fetchRandomCharacter } from '../../helper/fetchRandomCharacter';
 
-const RandomCharacter = () => {
-
-
-useEffect(() => {
-  
-}, )
-
-  return ( 
-    <>
-    <h2>Hello mysterious wanderer!</h2>
-    <p>Are you interested in getting some secrets?</p>
-    <button>Get a random character</button>
-    </>
-  )
+type Character = {
+  name: string;
+  description: string;
+  imageUrl: string;
 }
 
-export default RandomCharacter
+const RandomCharacter = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [buttonPressed, setButtonPressed] = useState<boolean>(false);
+  const [character, setCharacter] = useState<Character | null>(null);
+
+  const getRandomCharacterButtonHandler = async () => {
+    setIsLoading(true)
+    const fetchedCharacter = await fetchRandomCharacter();
+    setCharacter(fetchedCharacter);
+    if (!buttonPressed) {
+      setButtonPressed(true);
+    }
+    setIsLoading(false)
+  };
+
+  return (
+    <>
+      <div>
+        <h2>Hello mysterious wanderer!</h2>
+        <p>Are you interested in getting some secrets?</p>
+        <button onClick={getRandomCharacterButtonHandler}>
+          Get a random character
+        </button>
+      </div>
+      {isLoading && <Spinner />}
+      {buttonPressed && !isLoading && (
+        <CharacterCard>
+          <h1>{character?.name}</h1>
+          <img src={character?.imageUrl} alt={character?.name} />
+          <p>{character?.description}</p>
+        </CharacterCard>
+      )}
+    </>
+  );
+};
+
+export default RandomCharacter;
